b9ebeb66ee304b94c62e13109c6a54d71af68a86
Henderson Teow <hendersonteow@gmail.com>
2019-09-12 17:49:44 +0800

    trying to link

---
+++ foodymoody/src/pages/homepage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default class Homepage extends React.Component {
+    state = {
+        
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <h1>Welcome to FoodyMoody!</h1>
+                    <button>Play Game</button>
+                    <button>I JUST WANT TO EAT!</button>
+                    <button>Hidden Gems</button>
+                </div> 
+            </div>
+        )
+    }
+}
