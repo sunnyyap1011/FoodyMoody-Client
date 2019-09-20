40b3b829a6cde2fd8e9343657b936bc1a078db52
Luke <lukewong93@gmail.com>
2019-09-20 16:20:02 +0800

    added Google place search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 // Importing Section
-import React from 'react'
-import Homepage from './pages/homepage'
-import GameLobby from './pages/game-lobby' 
-import Create from './pages/create-lobby'
-import Random from './containers/random-game' 
-import GamePage from './containers/play-game'
-import Waiting from './pages/waiting-lobby'
-import { Route } from 'react-router-dom'
+import React from "react";
+import Homepage from "./pages/homepage";
+import GameLobby from "./pages/game-lobby";
+import Create from "./pages/create-lobby";
+import Random from "./containers/random-game";
+import GamePage from "./containers/play-game";
+import Waiting from "./pages/waiting-lobby";
+import { ToastContainer } from "react-toastify";
+import { Route, Switch } from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.min.css' 
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Route exact path = "/home" component = { Homepage } />
-        <Route path = "/random_restaurants" component = { Random } />
-        <Route path = "/create_lobby" component = { Create } />
-        <Route path = "/:room_id/game_lobby" component = { GameLobby } />
-        <Route path = "/:room_id/play_game" component = { GamePage } />
-        <Route path = "/:room_id/waiting_lobby" component = { Waiting } />
+        <ToastContainer />
+        <Switch>
+          <Route exact path="/home" component={Homepage} />
+          <Route path="/random_restaurants" component={Random} />
+          <Route path="/create_lobby" component={Create} />
+          <Route path="/:room_id/game_lobby" component={GameLobby} />
+          <Route path="/:room_id/play_game" component={GamePage} />
+          <Route path="/:room_id/waiting_lobby" component={Waiting} />
+        </Switch>
       </div>
-    )
+    );
   }
 }
 
