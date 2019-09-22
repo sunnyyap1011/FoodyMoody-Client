6118f705ccdd4a2e26d2278f8dc51ef4b36cdcbb
sunnyyap1011 <sunnyyap1011@gmail.com>
2019-09-23 01:18:37 +0800

    Redirect route '/' to '/home'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Random from "./containers/random-game";
 import GamePage from "./containers/play-game";
 import Waiting from "./pages/waiting-lobby";
 import { ToastContainer } from "react-toastify";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.min.css' 
 
 class App extends React.Component {
@@ -22,6 +22,7 @@ class App extends React.Component {
           <Route path="/:room_id/game_lobby" component={GameLobby} />
           <Route path="/:room_id/play_game" component={GamePage} />
           <Route path="/:room_id/waiting_lobby" component={Waiting} />
+          <Redirect from="/" to="/home" />
         </Switch>
       </div>
     );
