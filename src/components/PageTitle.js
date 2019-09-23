5aef6e6581695365ac673700c360f3b28d95268d
Luke <lukewong93@gmail.com>
2019-09-23 19:06:53 +0800

    design game setup

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,19 +1,19 @@
-// Importing Section
-import React from 'react'
-import styled from 'styled-components'
+// // Importing Section
+// import React from 'react'
+// import styled from 'styled-components'
 
-// Styling Section
-const Title = styled.h1`
-  font-family: 'Kavoon', cursive;
-  text-align: center;
-  font-size: 100px;
-`
+// // Styling Section
+// const Title = styled.h1`
+//   font-family: 'Kavoon', cursive;
+//   text-align: center;
+//   font-size: 100px;
+// `
 
-// Rendering Section
-const PageTitle = props => (
-  <Title>
-    {props.children}
-  </Title>
-)
+// // Rendering Section
+// const PageTitle = props => (
+//   <Title>
+//     {props.children}
+//   </Title>
+// )
 
-export default PageTitle
+// export default PageTitle
--- src/pages/game-lobby.js
+++ src/pages/game-lobby.js
@@ -1,10 +1,10 @@
 // Importing Section
 import React from "react";
 import styled from "styled-components";
-import { Redirect, Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import Socket from "../utils/socket";
-import PageTitle from "../components/PageTitle";
-import { Button, Form, Input } from "reactstrap";
+import { Button, Input } from "reactstrap";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import "../game-lobby.css";
 
 // Stylings Section
@@ -12,6 +12,30 @@ const LobbyBody = styled.div`
   background-color: #9dbde3;
   height: 100vh;
   text-align: center;
+  font-family: "Amatic SC", cursive;
+
+  .fas {
+    display: inline-block;
+    font-family: FontAwesome;
+  }
+
+  .fa-chevron-circle-left:before {
+    content: "\f137";
+  }
+
+  .copied {
+    font-size: 30px;
+    font-weight: bold;
+    color: red;
+    margin-left: 10px;
+  }
+
+  .share {
+    font-size: 40px;
+    font-weight: bold;
+    margin-left: 10px;
+    color: black;
+  }
 
   .room-info {
     margin: 1.2rem;
@@ -19,11 +43,12 @@ const LobbyBody = styled.div`
 
     h3 {
       font-weight: bold;
+      font-size: 90px;
     }
 
     p {
       font-family: monospace;
-      font-size: 17px;
+      font-size: 25px;
     }
   }
 
@@ -33,12 +58,11 @@ const LobbyBody = styled.div`
     label {
       margin-right: 0.5rem;
       font-weight: bold;
-      font-size: 17px;
+      font-size: 30px;
     }
   }
 
   form {
-
     h4 {
       font-weight: bold;
     }
@@ -52,7 +76,6 @@ const LobbyBody = styled.div`
       margin-top: 1.5rem;
     }
   }
-
 `;
 
 const RadioDiv = styled.div`
@@ -60,6 +83,22 @@ const RadioDiv = styled.div`
   color: black;
   font-family: "Helvetica Neue", "Helvetica", "Roboto", "Arial", sans-serif;
   text-align: center;
+`;
+
+const Setup = styled.div`
+  background-color: #dbe2ef;
+  width: 550px;
+  height: 360px;
+  border: solid black 3px;
+  margin: 20px auto 20px auto;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SetupTitle = styled.h2`
+  font-size: 50px;
+  font-weight: bold;
 `;
 
 // Components Section
@@ -71,10 +110,11 @@ export default class GameLobby extends React.Component {
       num_people: 1,
       room_id: this.props.location.state.room_id,
       redirect: false,
-      location: '',
-      rounds: '3',
-      restaurants: ''
-    }
+      location: "",
+      rounds: "3",
+      restaurants: "",
+      copied: false
+    };
     this.autocompleteInput = React.createRef();
     this.autocomplete = null;
     this.handlePlaceChanged = this.handlePlaceChanged.bind(this);
@@ -94,47 +134,55 @@ export default class GameLobby extends React.Component {
     Socket.on("on_leave", () => {
       this.setState({
         num_people: this.state.num_people - 1
-      })
-    })
-    this.autocomplete = new google.maps.places.Autocomplete(this.autocompleteInput.current,
-      { "types": ["geocode"], componentRestrictions: { country: 'my' } });
-    this.autocomplete.addListener('place_changed', this.handlePlaceChanged);
+      });
+    });
+    this.autocomplete = new google.maps.places.Autocomplete(
+      this.autocompleteInput.current,
+      { types: ["geocode"], componentRestrictions: { country: "my" } }
+    );
+    this.autocomplete.addListener("place_changed", this.handlePlaceChanged);
   }
 
-  handleSubmit = (e) => {
-    e.preventDefault()
-    const { location, rounds, room_id } = this.state
-    Socket.emit('conditions', { "lat": location.lat, "lng": location.lng, "rounds": rounds, "room": room_id })
+  handleSubmit = e => {
+    e.preventDefault();
+    const { location, rounds, room_id } = this.state;
+    Socket.emit("conditions", {
+      lat: location.lat,
+      lng: location.lng,
+      rounds: rounds,
+      room: room_id
+    });
     this.setState({
       redirect: true
     });
   };
 
-  handlePlaceChanged = (e) => {
+  handlePlaceChanged = e => {
     const place = this.autocomplete.getPlace();
     if (place.geometry) {
-      const lat = place.geometry.location.lat()
-      const lng = place.geometry.location.lng()
-      const name = place.name
+      const lat = place.geometry.location.lat();
+      const lng = place.geometry.location.lng();
+      const name = place.name;
 
       this.setState({
-        location: { "lat": lat, "lng": lng, "name": name },
+        location: { lat: lat, lng: lng, name: name },
         current_loc_alert: false
-      })
+      });
     } else {
-      console.log('Please select from Google dropdown')
+      console.log("Please select from Google dropdown");
     }
-  }
+  };
 
-  handleChangeLocation = (e) => {
-    e.preventDefault()
+  handleChangeLocation = e => {
+    e.preventDefault();
+    console.log(e.target);
     if (!e.target.value) {
       this.setState({
         location: e.target.value,
         alert_visible: false
-      })
+      });
     }
-  }
+  };
 
   handleChangeRound = e => {
     e.preventDefault();
@@ -159,9 +207,14 @@ export default class GameLobby extends React.Component {
 
   // Rendering Section
   render() {
+<<<<<<< HEAD
     const { num_people, room_id, location, rounds } = this.state
     console.log(rounds)
     console.log(location)
+=======
+    const { num_people, room_id, location, rounds } = this.state;
+    console.log(rounds);
+>>>>>>> design game setup
 
     if (this.state.redirect) {
       return this.renderRedirect();
@@ -169,59 +222,104 @@ export default class GameLobby extends React.Component {
 
     return (
       <LobbyBody>
-        <div>
-          <PageTitle>Set Up Game</PageTitle>
-        </div>
-
         <div className="room-info">
-          <small>Share this room id with your friends</small>
-          <h3>Room ID: {room_id}</h3>
-          <p>Players: {num_people}</p>
+          <h3>Welcome To Room "{room_id}"</h3>
+          <p>Player(s) In Room: {num_people}</p>
         </div>
 
-        <div id="location-container">
-          <label>Location:</label>
-          <input ref={this.autocompleteInput} onChange={this.handleChangeLocation} id="autocomplete" placeholder="keyword" type="text"></input>
-        </div>
+        <CopyToClipboard
+          text={this.state.room_id}
+          onCopy={() => this.setState({ copied: true })}
+        >
+          <Button
+            style={{
+              backgroundColor: "#48466D",
+              fontSize: "40px",
+              borderRadius: "20px",
+              marginBottom: "10px"
+            }}
+          >
+            Copy this room ID
+          </Button>
+        </CopyToClipboard>
 
+        {/* <span className="share">& share it with your friends!</span> */}
 
-        <form onSubmit={this.handleSubmit} className="m-3">
-          <RadioDiv>
-            <h4>Select Rounds&hellip;</h4>
-            <label>
-              <input
-                type="radio"
-                className="option-input radio"
-                name="example"
-                value="3"
-                defaultChecked
-              />
-              3 ROUNDS
-            </label>
-            <label>
-              <input
-                type="radio"
-                className="option-input radio"
-                name="example"
-                value="5"
-              />
-              5 ROUNDS
-            </label>
-            <label>
-              <input
-                type="radio"
-                className="option-input radio"
-                name="example"
-                value="8"
-              />
-              8 ROUNDS
-            </label>
-          </RadioDiv>
+        {this.state.copied ? (
+          <span className="copied">Copied to Clipboard!</span>
+        ) : (
+          <span className="share">& share it!</span>
+        )}
 
-          <Button type="submit" className="btn-success" disabled={!location}>PLAY!</Button>
-        </form>
+        <Setup>
+          <SetupTitle>Set Up Game</SetupTitle>
+          <div
+            style={{ marginBottom: "10px", marginTop: "15px" }}
+            id="location-container"
+          >
+            <label>Choose your location :</label>
+            <Input
+              innerRef={this.autocompleteInput}
+              onChange={this.handleChangeLocation}
+              id="autocomplete"
+              placeholder="Search location"
+              type="text"
+            ></Input>
+          </div>
 
-        <Button href={"/home"}>Back to HomePage</Button>
+          <form
+            onSubmit={this.handleSubmit}
+            className="m-3 d-flex align-items-center flex-column"
+          >
+            <RadioDiv>
+              <label>
+                <input
+                  type="radio"
+                  className="option-input radio"
+                  name="example"
+                  value="3"
+                  defaultChecked
+                />
+                3 ROUNDS
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  className="option-input radio"
+                  name="example"
+                  value="5"
+                />
+                5 ROUNDS
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  className="option-input radio"
+                  name="example"
+                  value="8"
+                />
+                8 ROUNDS
+              </label>
+            </RadioDiv>
+
+            <button id="play_btn" disabled={!location} type="submit">
+              PLAY!
+            </button>
+          </form>
+        </Setup>
+
+        <Button
+          style={{
+            backgroundColor: "#48466D",
+            fontSize: "40px",
+            borderRadius: "20px",
+            marginBottom: "10px"
+          }}
+          href={"/home"}
+        >
+          <i class="fas fa-chevron-circle-left"></i>
+          GO HOME
+        </Button>
       </LobbyBody>
     );
   }
